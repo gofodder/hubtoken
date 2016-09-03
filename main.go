b6f9c0dc89305ce28c54ecb39e66befe7b71de49
jasonm23 <jasonm23@gmail.com>
2016-09-03 23:30:28 +0800

    create command added
    - stubs for delete and list added to cli front end

--- main.go
+++ main.go
@@ -1,19 +1,87 @@
 package main
 
-import "os"
-import "github.com/codegangsta/cli"
+import (
+  "os"
+  "fmt"
+  "bufio"
+  "syscall"
+  "strings"
+  "golang.org/x/crypto/ssh/terminal"
+  "github.com/codegangsta/cli"
+  "github.com/google/go-github/github"
+)
+
+func prompt(Message string) string {
+  fmt.Print(Message)
+  reader := bufio.NewReader(os.Stdin)
+  text, _ := reader.ReadString('\n')
+  return strings.TrimSpace(text)
+}
+
+func password(Message string) string {
+  fmt.Print(Message)
+  bytes, _ := terminal.ReadPassword(int(syscall.Stdin))
+  text := string(bytes)
+  fmt.Println()
+  return strings.TrimSpace(text)
+}
 
 func main() {
+  app           := cli.NewApp()
+  app.Name       = "HubToken"
+  app.Usage      = "Manage GitHub personal access tokens"
+  app.Version    = "0.1.0"
+  app.Commands   = []cli.Command{
+    {
+      Name: "create",
+      Aliases: []string{"c"},
+      Usage: "Create personal access token called `NAME`",
+      Action: func(c *cli.Context) error {
+        note      := c.Args().First()
+        login     := prompt("Github login: ")
+        password  := password("password: ")
+        otp       := prompt("2FA/OTP: ")
+        scopes    := []github.Scope{"repo"}
+        tp        := github.BasicAuthTransport{Username: login, Password: password, OTP: otp}
+        client    := github.NewClient(tp.Client())
+        input     := &github.AuthorizationRequest{Note: &note, Scopes: scopes}
 
-  app := cli.NewApp()
-  app.Name = "HubToken"
-  app.Usage = "Manage GitHub personal access tokens"
-  app.Action = func(c *cli.Context) {
-    //
+        authorization, _, err := client.Authorizations.Create(input)
+
+        if err != nil {
+          fmt.Errorf("Authorizations.Create returned error: %v", err)
+          return nil
+        }
+
+        token := *authorization.Token
+        fmt.Printf("%v\n", strings.TrimPrefix(token, "0x"))
+
+        return nil
+      },
+    },
+    {
+      Name: "delete",
+      Aliases: []string{"D"},
+      Usage: "Delete a token called `NAME`",
+      Action: func(c *cli.Context) error {
+        // Request Authentication Parameters (Login, Password, OTP)
+        // Call Github API
+        fmt.Println("deleting ...", c.Args().First())
+        return nil
+      },
+    },
+    {
+      Name: "list",
+      Aliases: []string{"l"},
+      Usage: "list personal access tokens",
+      Action: func(c *cli.Context) error {
+        // Request Authentication Parameters (Login, Password, OTP)
+        // Call Github API
+        fmt.Println("listing ...")
+        return nil
+      },
+    },
   }
 
-  // createOpt := flag.String("create", "token-name", "create a personal access token called `token-name`")
-  // flag.Parse()
-  // fmt.Println("create token: ", *createOpt)
-
+  app.Run(os.Args)
 }
